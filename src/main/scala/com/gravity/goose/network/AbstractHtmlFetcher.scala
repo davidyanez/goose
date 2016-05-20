4000277a4089d5a24832e9adbd3269b688a5dcab
davidy <davidy@stradigi.ca>
2016-05-20 18:19:43 -0400

    created getContentCharSet() method to extract the CharSet with 3 different methods

--- src/main/scala/com/gravity/goose/network/AbstractHtmlFetcher.scala
+++ src/main/scala/com/gravity/goose/network/AbstractHtmlFetcher.scala
@@ -2,9 +2,10 @@ package com.gravity.goose.network
 
 import com.gravity.goose.Configuration
 import org.apache.http.client.HttpClient
+import org.jsoup.nodes.Document
 
 /**
- * Created by IntelliJ IDEA.
+  * Created by IntelliJ IDEA.
  * Author: Robbie Coleman
  * Date: 10/13/12
  * Time: 1:02 AM
@@ -14,15 +15,19 @@ import org.apache.http.client.HttpClient
 trait AbstractHtmlFetcher {
   /**
    * Access the `url` over the internet and retrieve the HTML from it
-   * @param config overrides and tweaks
+    *
+    * @param config overrides and tweaks
    * @param url the address to access and retrieve content from
    * @return `Some` `String` of the response from the specified `url` or `None` if failed to retrieve HTML.
    */
   def getHtml(config: Configuration, url: String): Option[String]
 
+  def getCharSet(doc: Document): String
+
   /**
    * A shared accessor for making image calls
-   * @return a fully configured and initialized instance for shared use
+    *
+    * @return a fully configured and initialized instance for shared use
    */
   def getHttpClient: HttpClient
 }
