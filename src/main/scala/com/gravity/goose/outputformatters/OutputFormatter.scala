3e76370de9446066941d85e72188363e431c2def
davidy <davidy@stradigi.ca>
2016-07-13 15:45:36 -0400

    modified the article footer

--- src/main/scala/com/gravity/goose/outputformatters/OutputFormatter.scala
+++ src/main/scala/com/gravity/goose/outputformatters/OutputFormatter.scala
@@ -249,8 +249,11 @@ trait OutputFormatter {
 
 
      val article_footer =
-       s"<hr><a class='link' href='${article.finalUrl}' target='_blank'>Read on ${article.domain}</a>" +
-       s"<div class='scraped_date'>Pulled on $today_str </div>"
+       "<div class=\"reflink\">"+ "<span> Read on the original source </span>"
+       s"<a class='link' href='${article.finalUrl}' target='_blank'>Read on ${article.domain}</a>" +
+       s"<div class='scraped_date'>Pulled on $today_str </div>"+
+       s"</div>"
+      ""
 
      article_div_html + article_footer
    }
